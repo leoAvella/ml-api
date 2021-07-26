9e494e8817efc9f73534b0a3c5340b06c9fc4454
leonardoavella <dleonardoavella@gmail.com>
2021-07-25 20:00:31 -0500

    view search

--- src/App.js
+++ src/App.js
@@ -5,47 +5,56 @@ import logo from './Logo_ML@2x.png.png.png';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import {Modal,ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
-import {Navbar,  Container, NavDropdown, Button, Form, FormControl, Image} from 'react-bootstrap';
+import {Navbar,  Container, FormControl, Image} from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { library } from "@fortawesome/fontawesome-svg-core";
 library.add(faSearch);
 
-const url = "https://api.mercadolibre.com/sites/MLA/search?q=";
+const url = "http://localhost:8888/api/items";
 
 class App extends Component{
 
-  state = {
-      data:[],
-      qSearch: ''
+    constructor (props) {
+        super(props)
+        this.state = {
+            data:{items:[], categories: []},
+            qSearch: ''
+        }
+        this.searchChange = this.searchChange.bind(this);
+    }
+
+
+  headers(){
+        return {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
   }
 
   getSearch(qSearch) {
-    axios.get(url+qSearch).then(response =>{
-      console.log(response.data);
+    axios.get(url+"?q="+qSearch, {headers: this.headers()}).then(response =>{
+        this.setState({data: response.data});
+        console.log("....",this.state.data.categories);
+    }).catch(error=>{
+        console.log(error);
     })
   }
 
 
+
   searchChange(e){
-      const qSearch = e.target.value;
-      //this.getSearch(qSearch);
-      axios.get(url+qSearch).then(response =>{
-          console.log(response.data);
-      })
-      //if ( qSearch.trim() != ''){   this.getSearch(qSearch);
+      this.getSearch(e.target.value);
   }
 
   componentDidMount(){
-
   }
 
 
   render(){
       return (
-          <div className="App">
+          <div className="main">
 
               <Navbar className="nav-header">
                   <Container>
@@ -77,7 +86,42 @@ class App extends Component{
 
                   </Container>
               </Navbar>
+              <div className="main">
 
+                  <Container>
+                      <br/>
+                      <p className="breadcrumb">{
+                          this.state.data.categories.map( category => {
+                              return(<span key={category}>{category} > </span>)
+                          })
+                      }</p>
+
+                      <div className="card">
+                          <ul className="list-group list-group-flush">
+                              {
+                                  this.state.data.items.map( item => {
+                                    return(
+                                        <li key={item.id} className="list-group-item">
+                                            <div className="row">
+                                                <div className="col-md-2">
+                                                    <Image  src={item.picture} className="picture-item"/>
+                                                </div>
+                                                <div className="col-md-7">
+                                                    <h6>$ { Intl.NumberFormat('es-ES').format(item.price.amount) }</h6>
+                                                    <h8>{item.title}</h8>
+                                                </div>
+                                                <div className="col-md-3">
+                                                    <span>{item.price.currency}</span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                  })
+                              }
+                          </ul>
+                      </div>
+                  </Container>
+              </div>
           </div>
       );
   }
